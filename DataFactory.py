1eb658c208a5c6d8d600f06e3ab7a8a62cf743a1
PHRupp <pat.h.rupp@gmail.com>
2020-08-11 21:40:23 -0500

    Added time test

--- DataFactory.py
+++ DataFactory.py
@@ -10,17 +10,65 @@ class DataFactory():
 			np.random.seed( self.seed )
 		return
 	
-	def create_data_helix( self, num_points: int, rand_weight: int, t_max: int = 20 ):
-		t = np.linspace(0, t_max, num_points)
-		x = np.cos(t) + rand_weight * random( num_points )
-		y = np.sin(t) + rand_weight * random( num_points )
-		z = 2*t + rand_weight * random( num_points )
-		X = np.hstack([
-			t.reshape((num_points,1)),
-			x.reshape((num_points,1)),
-			y.reshape((num_points,1)),
-			z.reshape((num_points,1))
+	def create_data_helix( self, num_points: int, rand_weight: float, t_max: int = 20 ):
+		n1 = int(num_points/2)
+		n2 = num_points - n1
+		
+		t1 = np.linspace(0, t_max, n1) + 0.5 * random( n1 )
+		x1 = np.cos(t1) + rand_weight * random( n1 )
+		y1 = np.sin(t1) + rand_weight * random( n1 )
+		z1 = 2*t1 + rand_weight * random( n1 )
+		X1 = np.hstack([
+			t1.reshape((n1,1)),
+			x1.reshape((n1,1)),
+			y1.reshape((n1,1)),
+			z1.reshape((n1,1))
 		])
+		
+		t2 = np.linspace(0, t_max, n2) + 0.5 * random( n2 )
+		x2 = np.cos(t2) + rand_weight * random( n2 )
+		y2 = np.sin(t2) + rand_weight * random( n2 )
+		z2 = 2*t2 + rand_weight * random( n2 )
+		X2 = np.hstack([
+			t2.reshape((n2,1)),
+			x2.reshape((n2,1)),
+			y2.reshape((n2,1)),
+			z2.reshape((n2,1))
+		])
+		
+		X = np.vstack([X1,X2])
+		
+		return X
+		
+	def create_sine_wave( self, num_points: int, rand_weight: float, x_max: int = 20):
+	
+		n1 = int(num_points/2)
+		n2 = num_points - n1
+		
+		x1 = np.linspace(0, x_max, n1) + 0.5 * random( n1 )
+		y1 = np.linspace(0, x_max, n1) + 0.5 * random( n1 )
+		z1 = np.sin( np.sqrt( np.power(x1,2) + np.power(y1,2) ))
+		t1 = np.linspace(0, t_max, n1)
+		X1 = np.hstack([
+			x1.reshape((n1,1)),
+			y1.reshape((n1,1)),
+			z1.reshape((n1,1)),
+			t1.reshape((n1,1))
+		])
+		
+		x2 = np.linspace(0, x_max, n2)
+		y2 = np.sin(t2) + rand_weight * random( n2 )
+		z2 = 2*t2 + rand_weight * random( n2 )
+		t2 = np.linspace(0, t_max, n2)
+		X2 = np.hstack([
+			x2.reshape((n2,1)),
+			y2.reshape((n2,1)),
+			z2.reshape((n2,1)),
+			t2.reshape((n2,1))
+		])
+		
+		X = np.vstack([X1,X2])
+	
 		return X
 	
 	def create_data_S( self, num_points: int, rand_weight: int ):
---
+++ time_test.py
@@ -0,0 +1,119 @@
+#author: patrick h. rupp
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+
+from datetime import datetime as dt
+from mpl_toolkits.mplot3d import Axes3D
+from pydiffmap import diffusion_map
+from pydiffmap.visualization import embedding_plot, data_plot
+
+# include self-built library
+from DataFactory import DataFactory
+from DiffusionMapsKD import DiffusionMapsKD
+from DiffusionMapsPat import DiffusionMapsPat
+from Utility import get_pid_memory_usage
+
+# define parameters
+seed = 12345
+rand_weight = 1
+new_dimensions = 2
+alpha = 3.0
+num_points = [int(i) for i in np.linspace(10, 2000, 10)]
+
+data_factory = DataFactory( seed )
+
+diff_map = diffusion_map.DiffusionMap.from_sklearn(
+	n_evecs = new_dimensions,
+	epsilon = 1.0,
+	alpha = alpha,
+	k = 10, #number of neighbors to use
+	neighbor_params={
+		'n_jobs': -1,
+		'algorithm': 'ball_tree'
+	}
+)
+
+time_df = pd.DataFrame(
+	index=[i for i in range(len(num_points))], 
+	columns=['sklearn', 'kdnuggets', 'kdnuggets_pat']
+	)
+
+j = 0
+
+for n in num_points:
+
+	# calculate the fields
+	X = data_factory.create_data_helix( n, rand_weight, t_max=20 )
+
+	# run
+	Xp1_start_time = dt.now()
+	Xp1 = diff_map.fit_transform(X)
+	time_df.loc[j, 'sklearn'] = (dt.now() - Xp1_start_time).total_seconds()
+
+	Xp2_start_time = dt.now()
+	Xp2 = DiffusionMapsKD.fit_transform(X, new_dimensions, alpha)
+	time_df.loc[j, 'kdnuggets'] = (dt.now() - Xp2_start_time).total_seconds()
+
+	Xp3_start_time = dt.now()
+	Xp3 = DiffusionMapsPat.fit_transform(X, new_dimensions, alpha)
+	time_df.loc[j, 'kdnuggets_pat'] = (dt.now() - Xp3_start_time).total_seconds()
+	
+	j+=1 #increment
+
+plt.plot( num_points, time_df['sklearn'], color='red', label='sklearn' )
+plt.plot( num_points, time_df['kdnuggets'], color='green', label='kdnuggets' )
+plt.plot( num_points, time_df['kdnuggets_pat'], color='blue', label='kdnuggets_pat' )
+plt.title('Computation Time vs. Data Size')
+plt.xlabel('Data Size (n)')
+plt.ylabel('Computation Time (s)')
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
